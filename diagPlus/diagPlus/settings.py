f71d164a39b8397dff82b69d433c2c8f9243fbaf
Alexandre TO <alexandreto1996@gmail.com>
2021-10-20 16:57:50 +0200

    Security Added Bootstrap on the user view

--- diagPlus/diagPlus/settings.py
+++ diagPlus/diagPlus/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+!q58b#z5k9qfy^u&@v1s49+rpq9jtt1x#!dl+m*%!=++gz4k9'
+# NEVER PUT YOUR SECRET KEY HERE. Put it in a .env file 
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
