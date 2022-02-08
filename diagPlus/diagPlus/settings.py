b69cf91bdd004ebdfb3958925893155cccd9af03
Alexandre TO <alexandreto1996@gmail.com>
2022-02-08 16:33:59 +0100

    cors

--- diagPlus/diagPlus/settings.py
+++ diagPlus/diagPlus/settings.py
@@ -153,7 +153,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework_simplejwt.authentication.JWTAuthentication',)
 }
 
-CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
 
 CORS_ORIGIN_WHITELIST = [
     'https://localhost:3000'
