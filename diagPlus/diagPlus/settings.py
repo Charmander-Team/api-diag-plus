45e6a907eeea5393c99e1d9079318e4afc9c261a
Alexandre TO <alexandreto1996@gmail.com>
2022-02-08 18:30:40 +0100

    testing

--- diagPlus/diagPlus/settings.py
+++ diagPlus/diagPlus/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import environ
 from datetime import timedelta
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
