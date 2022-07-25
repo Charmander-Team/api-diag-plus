9d301c334938f804cc8c77ccea667af461df8c7c
Alexandre TO <alexandreto1996@gmail.com>
2022-07-25 15:39:56 +0200

    add receiver in signals

--- diagPlus/Api/signals.py
+++ diagPlus/Api/signals.py
@@ -3,11 +3,16 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
 from django.contrib.auth.models import User
+from .models import Patient, Praticien
+
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
+@receiver(post_save, sender=Patient)
+@receiver(post_save, sender=Praticien)
 def create_auth_token(sender, instance=None, created=False, **kwargs):
     if created:
         Token.objects.create(user=instance)
-        
+
+
 for user in User.objects.all():
     Token.objects.get_or_create(user=user)
