f8facc9efb6b5daf609e78632382217dfa56d560
Alexandre TO <alexandreto1996@gmail.com>
2022-07-25 09:42:34 +0200

    add permissions for the user endpoint

--- diagPlus/Api/permissions.py
+++ diagPlus/Api/permissions.py
@@ -1,7 +1,29 @@
 from rest_framework.permissions import BasePermission
+from rest_framework import permissions
 
 
 class isAdminAuthenticated(BasePermission):
     # check if the user authenticated is an admin
     def has_permission(self, request, view):
         return bool(request.user and request.user.is_authenticated and request.user.is_superuser)
+
+
+class isOwnerOrReadOnly(BasePermission):
+    # check if the user is the owner of the object
+    edit_methods = ("PUT", "PATCH")
+
+    def has_permission(self, request, view):
+        if request.user.is_authenticated:
+            return True
+
+    def has_object_permission(self, request, view, obj):
+        if request.user.is_superuser:
+            return True
+
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        if obj.author == request.user:
+            return True
+
+        return False
--- diagPlus/Api/views.py
+++ diagPlus/Api/views.py
@@ -1,90 +1,108 @@
-from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly, AllowAny
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView
 from .models import *
 from .serializers import *
 from rest_framework import viewsets
-from .permissions import isAdminAuthenticated
+from .permissions import isAdminAuthenticated, isOwnerOrReadOnly
 
 # API Views
+
+
 class ListUser(viewsets.ModelViewSet):
     serializer_class = UserSerializer
+    permission_classes = [isOwnerOrReadOnly]
     queryset = User.objects.all()
+
 
 class ListSpeciality(viewsets.ModelViewSet):
     serializer_class = SpecialitySerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Speciality.objects.all()
 
+
 class ListQuestion(viewsets.ModelViewSet):
     serializer_class = QuestionSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Question.objects.all()
+
 
 class ListAttachment(viewsets.ModelViewSet):
     serializer_class = AttachementSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Attachment.objects.all()
 
+
 class ListPatient(viewsets.ModelViewSet):
     serializer_class = PatientSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Patient.objects.all()
+
 
 class ListAdmin(viewsets.ModelViewSet):
     serializer_class = AdminSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Admin.objects.all()
 
+
 class ListPraticien(viewsets.ModelViewSet):
     serializer_class = PraticienSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Praticien.objects.all()
+
 
 class ListFiles(viewsets.ModelViewSet):
     serializer_class = FileSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Files.objects.all()
 
+
 class ListReportPatient(viewsets.ModelViewSet):
     serializer_class = ReportPatientSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = ReportPatient.objects.all()
+
 
 class ListAppointment(viewsets.ModelViewSet):
     serializer_class = AppointmentSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Appointment.objects.all()
 
+
 class ListPlanning(viewsets.ModelViewSet):
     serializer_class = PlanningSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Planning.objects.all()
+
 
 class ListDiagnostic(viewsets.ModelViewSet):
     serializer_class = DiagnosticSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Diagnostic.objects.all()
 
+
 class ListResponse(viewsets.ModelViewSet):
     serializer_class = ResponseSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Response.objects.all()
+
 
 class ListPathology(viewsets.ModelViewSet):
     serializer_class = PathologySerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Pathology.objects.all()
 
+
 class ListReason(viewsets.ModelViewSet):
     serializer_class = ReasonSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Reason.objects.all()
 
+
 class ListSymptom(viewsets.ModelViewSet):
     serializer_class = SymptomSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Symptom.objects.all()
+
 
 class AdminUser(viewsets.ModelViewSet):
     serializer_class = UserSerializer
@@ -93,12 +111,14 @@ class AdminUser(viewsets.ModelViewSet):
 
     def get_queryset(self):
         return User.objects.all()
+
+
 class GetTokenPair(TokenObtainPairView):
     permission_classes = (AllowAny,)
     serializer_class = TokenObtainPairSerializer
+
 
 class ListArticle(viewsets.ModelViewSet):
     serializer_class = ArticleSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Article.objects.all()
- 
